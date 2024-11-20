777b70843f8eccd4fee6478084fe6546a24e25c2
adish17 <adishirfan17@gmail.com>
2024-11-20 22:37:25 +0530

    Add model classes

---
+++ app/src/main/java/com/example/themovieapp/model/Movie.java
@@ -0,0 +1,67 @@
+package com.example.themovieapp.model;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+}
