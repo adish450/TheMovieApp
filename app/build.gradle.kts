49ac099e7f52db500fc1adcca233ca86b703c9ab
adish17 <adishirfan17@gmail.com>
2024-11-21 23:17:55 +0530

    adding views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.swiperefreshlayout)
     implementation(libs.constraintlayout)
     implementation(libs.javax.annotation.api)
     testImplementation(libs.junit)
