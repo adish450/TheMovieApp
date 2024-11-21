05c6b67edca368251f5b04683b4da03657aabe88
adish17 <adishirfan17@gmail.com>
2024-11-21 22:45:21 +0530

    adding viewmodel

--- app/src/main/java/com/example/themovieapp/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/example/themovieapp/viewmodel/MainActivityViewModel.java
@@ -1,2 +1,28 @@
-package com.example.themovieapp.viewmodel;public class MainActivityViewModel {
+package com.example.themovieapp.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.themovieapp.model.Movie;
+import com.example.themovieapp.model.MovieRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel { // AndroidViewModel is used to get context
+
+    MovieRepository movieRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.movieRepository = new MovieRepository(application);
+    }
+
+    public LiveData<List<Movie>> getPopularMovies() {
+        return movieRepository.getPopularMovies();
+    }
+
 }
